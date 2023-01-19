64289f6d354541aee4725abb6a12a7189809ba60
Jakob Sjöholm <jakob.sjoholm@live.se>
2023-01-19 13:26:36 +0100

    working now

--- app.ts
+++ app.ts
@@ -1,23 +1,30 @@
 
-
-const url = 'https://picsum.photos/v2/list?page=2&limit=100';
-const imageElement = document.createElement('img') as HTMLImageElement;
-const docu = document.querySelector('document') as HTMLElement;
+console.log("hej");
+const url = 'https://picsum.photos/v2/list';
+const imageElement = document.createElement("img") as HTMLImageElement;
 
 async function getPics() {
     const response = await fetch(url);
     const data = await response.json();
     return data;
 }
+getPics();
 
 
 function displayImg () {
     console.log("loaded...")
     const picDiv = document.querySelector(".container") as HTMLElement;
     getPics().then((data) => {
-        imageElement.src = data.url;
+        console.log(data)
+        imageElement.src = data[0].url;
         picDiv.append(imageElement);
-    });
+    })
 };
 
 displayImg();
+
+
+
+
+//tdeAYH8x6N9wVue4xBMgy5vdIgDF64KMT1HvrepyqSQ
+
